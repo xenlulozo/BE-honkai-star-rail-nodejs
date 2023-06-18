5e6909ccfcef21af054d0ccd17a7a500b677165b
hieu <baymaxvipx@gmail.com>
2023-06-18 20:52:24 +0700

    fix deploy api

--- src/routes/web.js
+++ src/routes/web.js
@@ -12,19 +12,32 @@ let initWebRoutes = (app) => {
     // return res.render("hse.ejs", { data: row });
     return res.send(JSON.stringify(row));
   });
+  router.get("/skills/:id", async (req, res) => {
+    const { id } = req.params;
+
+    const [row, fields] = await pool.execute(
+      `select * from skill where characterid =${id};`
+    );
+    // console.log(JSON.stringify(row));
+
+    return res.send(JSON.stringify(row));
+  });
   router.get("/traces/:id", async (req, res) => {
     const { id } = req.params;
     const [row, fields] = await pool.execute(
-      `SELECT CONCAT('[', GROUP_CONCAT( JSON_OBJECT( 'req', cts.req, 'desc', cts.description, 'name', cts.name, 'sub_nodes', IFNULL(subnodes_json, '[]') ) SEPARATOR ','), ']') AS traces FROM CharacterTraces cts LEFT JOIN ( SELECT ctsn.traceId, CONCAT('[', GROUP_CONCAT( JSON_OBJECT( 'req', ctsn.req, 'stat', ctsn.stat, 'value', ctsn.value ) SEPARATOR ','), ']') AS subnodes_json FROM CharacterTracesSubNodes ctsn GROUP BY ctsn.traceId ) subnodes ON cts.traceId = subnodes.traceId WHERE cts.characterId = ${id}  GROUP BY cts.traceId;`
+      `SELECT CONCAT('[', GROUP_CONCAT( JSON_OBJECT( 'req', cts.req, 'desc', cts.description, 'name', cts.name, 'sub_nodes', IFNULL(subnodes_json, '[]') ) SEPARATOR ','), ']') AS traces FROM charactertraces cts LEFT JOIN ( SELECT ctsn.traceId, CONCAT('[', GROUP_CONCAT( JSON_OBJECT( 'req', ctsn.req, 'stat', ctsn.stat, 'value', ctsn.value ) SEPARATOR ','), ']') AS subnodes_json FROM charactertracessubnodes ctsn GROUP BY ctsn.traceId ) subnodes ON cts.traceId = subnodes.traceId WHERE cts.characterId = ${id}  GROUP BY cts.traceId;`
     );
     // return res.render("hse.ejs", { data: row });
     return res.send(JSON.stringify(row));
   });
   router.get("/eidolon/:id", async (req, res) => {
     const { id } = req.params;
+
     const [row, fields] = await pool.execute(
       `SELECT JSON_OBJECT( 'eidolonId', eidolonId, 'upgrade1Name', upgrade1Name, 'upgrade1Desc', JSON_OBJECT('raw', upgrade1Desc), 'upgrade2Name', upgrade2Name, 'upgrade2Desc', JSON_OBJECT('raw', upgrade2Desc),'upgrade3Name', upgrade3Name, 'upgrade3Desc', JSON_OBJECT('raw', upgrade3Desc), 'upgrade4Name', upgrade4Name, 'upgrade4Desc', JSON_OBJECT('raw', upgrade4Desc), 'upgrade5Name', upgrade5Name, 'upgrade5Desc', JSON_OBJECT('raw', upgrade5Desc),'upgrade6Name', upgrade6Name,'upgrade6Desc', JSON_OBJECT('raw', upgrade6Desc) ) AS eidolon FROM eidolon where id =${id};`
     );
+    // console.log(JSON.stringify(row));
+
     return res.send(JSON.stringify(row));
   });
   router.get("/list", async (req, res) => {
